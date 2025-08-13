fcbab0eb37a2051f502f47637070e7116e508c35
lojadaconstrucao <adanbribeiro@yahoo.com>
2025-08-13 13:33:58 -0300

    icone

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "393150036"
+    serviceWorkerVersion: "3580361370"
   }
 });
